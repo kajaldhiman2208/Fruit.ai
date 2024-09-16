887fd11fbce0722a1c8358780a6ef020f0377636
kajaldhiman2208 <101688440+kajaldhiman2208@users.noreply.github.com>
2024-09-16 13:32:58 +0530

    Delete src directory

--- src/components/ui/Navigation.js
+++
@@ -1,9 +0,0 @@
-import { NavigationMenuDemo } from './components/NavigationMenuDemo'
-
-export default function Home() {
-  return (
-    <div className="container mx-auto p-6">
-      <NavigationMenuDemo />
-    </div>
-  )
-}
