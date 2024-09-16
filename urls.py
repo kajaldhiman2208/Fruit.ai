09f09d7e0e88f407edb981ff6225ce269864cdc9
kajaldhiman2208 <101688440+kajaldhiman2208@users.noreply.github.com>
2024-09-16 13:33:44 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import get_data
-from .views import ChatbotAPIView
-
-urlpatterns = [
-    path('data/', get_data),
-    path('chat/', ChatbotAPIView.as_view(), name='chat'),
-]
